edabeceed3f2cf490bac1b92e777e9a48a54068c
javierP1647 <javierfeb22@gmail.com>
2021-11-01 12:43:39 -0400

    axis hack

--- src/script.js
+++ src/script.js
@@ -1,26 +1,20 @@
 import * as THREE from "three/build/three.module.js";
-
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-
-//import OrbitWrapper from "../../node_modules/three/examples/jsm/controls/OrbitControls.js";
-
-//import "../../node_modules/three/examples/js/controls/OrbitControls.js";
-
+import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js';
 import * as dat from "dat.gui";
-
-//const { OrbitControls } = OrbitWrapper;
-
 import "./style.css";
 
 const objects = [];
-
+//const objGroup = new THREE.Group()
+const lastObject = objects[objects.length -1]
+let posIn3d
+let currentColor = objects.length > 1? objects[objects.length - 1].material.color : 0xff0000
 let camera, scene, renderer;
 let plane,
   controls,
   gui,
   pointer,
   raycaster,
-  isShiftDown = false,
   currentShape = "box",
   isCreationMode = false,
   createParams = {
@@ -28,24 +22,24 @@ let plane,
     geo: {},
   },
   parameters = {
-    color: 0xff0000,
-    create: () => assignParametersToCreate(), //createObject(),
-    deleteLast: () => objects.pop(),
-    clear: () => objects.clear(),
+    color: currentColor,
+    create: () => assignParametersToCreate(),
+    delete: () => deleteObject(),
+    clear: () => clearAllObject(),
+    resetCamera: () => resetCameraPosition(),
+    posUp: ()=> toScreenSpin('up'),
+    posDown: ()=> toScreenSpin('down'),
+    posLeft: ()=> toScreenSpin('left'),
+    posRight: ()=> toScreenSpin('right'),
+    rotateY: 'spin',
+    rotateZ: ()=> axisBack(),
   };
 
-let rollOverMesh, rollOverMaterial;
-let cubeGeo, cubeMaterial;
+const currentObject = () => objects.length > 0 ? objects[objects.length - 1] : null;
 
-const currentObject = () => {
-  const result = objects.length > 0 ? objects[objects.length - 1] : null;
 
-  //console.log("CHECKING CURRENT OBJECT", currentObject, objects, objects.children.length)
 
-  return result;
-};
-
-/* SHAPE TRANSLATION */
+/* SHAPE OPTIONS */
 
 function getShape(shape) {
   switch (shape) {
@@ -63,14 +57,15 @@ function getShape(shape) {
 /* SET PARAMETER OF OBJECT TO CREATE */
 
 function assignParametersToCreate() {
-  isCreationMode = true;
-  createControls();
+
+    createControls();
+    controls.enabled = false
 
   console.log("CHECKING PARAMETER ASSIGNEMENT", isCreationMode);
   document.addEventListener("pointerdown", onPointerDown);
 }
 
-/* CREATE OBECTE */
+/* OBJECT CRUD */
 
 function createObject(intersect) {
   const { x, y, z } = intersect.point;
@@ -83,34 +78,130 @@ function createObject(intersect) {
     color: createParams.color,
   });
   const basicMesh = new THREE.Mesh(basicGeometry, basicMaterial);
-  basicMesh.position.copy(intersect.point); //.position.set(x,y,z).add( intersect.face.normal );
-  //basicMesh.position.divideScalar( 50 ).floor().multiplyScalar( 50 ).addScalar( 25 );
+  basicMesh.position.copy(intersect.point);
 
   console.log("CHECKING BASIC MESH BEFORE PUSHING", basicMesh);
   scene.add(basicMesh);
   objects.push(basicMesh);
 
   console.log("CHECKING ALL OBJECTS AFTER CREATION", objects, scene);
+  controls.enabled = true
+}
+
+function deleteObject() {
+    if (objects.length > 1) {
+
+    scene.remove(objects[objects.length-1])
+    objects.pop()
+
+    }
+    createGui();
+}
+
+function clearAllObject() {
+    console.log(objects)
+    objects.forEach((obj, index)=>{
+        if (index >= 1){
+            scene.remove(obj)
+        }
+    })
+   objects.splice(1, objects.length-1)
+    console.log(objects)
 }
 
 // CONTROL CREATION
+function resetCameraPosition() {
+    //camera.position.set(0, 1500, 0);
+    //camera.rotation.set(0, 0, 0);
+    //camera.lookAt(0, 0, 0);
+    controls.reset();
+
+}
 
 function createControls() {
   controls = new OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
-  //controls.maxPolarAngle = Math.PI;
-  //controls.minPolarAngle = Math.PI;
+  controls.maxPolarAngle = Math.PI * -1;
+  controls.minPolarAngle = Math.PI * -1;
   controls.rotateSpeed = 0.1;
   controls.zoomSpeed = 0.1;
+  controls.maxDistance = 2000;
   controls.mouseButtons = {
     LEFT: THREE.MOUSE.PAN,
     MIDDLE: THREE.MOUSE.DOLLY,
     RIGHT: THREE.MOUSE.ROTATE,
   };
-  controls.enabled = !isCreationMode;
+  controls.enabled = true
+  //controls.addEventListener('change', axisSwap);
 }
 
-/* GUI CREEATION */
+/* SCENE CREATION */
+
+scene = new THREE.Scene();
+scene.background = new THREE.Color(0x000000);
+const objGroup = new THREE.Group()
+scene.add(objGroup)
+
+
+
+///// AXIS HACK /////
+
+
+const axisGeometry = new THREE.BoxGeometry( 100, 100, 250 );
+const axisMaterial = new THREE.MeshBasicMaterial( {color: 0x00ff00, wireframe: true} );
+const axisCube = new THREE.Mesh( axisGeometry, axisMaterial );
+axisCube.rotation.x = Math.PI * 1
+axisCube.rotation.y = Math.PI * 8
+axisCube.rotation.z = Math.PI / 1
+console.log(axisCube.rotation.y)
+scene.add( axisCube );
+console.log(axisCube)
+
+
+const axisBack = ()=> {
+  let spin = camera.rotation.z
+  let unSpin = camera.rotation.z * -1
+  axisCube.rotation.y = unSpin
+}
+
+/* AXIS SWAP */
+
+//controls.addEventListener('change', render);
+
+function axisSwap(){
+    axisCube.add(currentObject())
+    console.log(objGroup)
+    objGroup.add(currentObject())
+    console.log(objGroup)
+    objGroup.remove(currentObject())
+}
+
+////// TRANSLATE CONTROLS ////
+
+// const controller = new TransformControls( Camera, renderer.domElement );
+// controller.addEventListener( 'change', render );
+//
+// controller.addEventListener( 'dragging-changed', function ( event ) {
+//
+//    controls.enabled = ! event.value;
+//
+// } );
+
+
+
+const localGeometry = new THREE.BoxGeometry( 100, 100, 250 );
+const localMaterial = new THREE.MeshBasicMaterial( {color: 0x00CCCC, wireframe: true} );
+const localCube = new THREE.Mesh( localGeometry, localMaterial );
+axisCube.rotation.x = Math.PI * 1
+axisCube.rotation.y = Math.PI * 8
+axisCube.rotation.z = Math.PI / 1
+console.log(localCube.rotation.y)
+scene.add( localCube );
+
+console.log(localCube);
+
+/* GUI CREATION */
+
 const createGui = () => {
   /**
    * Panel
@@ -121,16 +212,20 @@ const createGui = () => {
   });
   gui.domElement.id = "gui";
 
-  // gui.hide()
 
-  //gui.add(initMesh, 'visible')
-  //gui.add(initMaterial, 'wireframe')
+  gui.add(parameters, "resetCamera");
+  gui.add(parameters, "rotateY");
+  gui.add(parameters, "rotateZ");
+  gui.add(parameters, "posUp");
+  gui.add(parameters, "posDown");
+  gui.add(parameters, "posLeft");
+  gui.add(parameters, "posRight");
 
   let create = gui.addFolder("Create New Object");
   create.open();
   create.addColor(createParams, "color").onChange(() => {
     //createParams.Color.set(parameters.color)
-    currentColor = createParams.Color;
+      currentColor = createParams.Color;
   });
   let changeGeo = create
     .add(createParams, "geo", {
@@ -150,36 +245,42 @@ const createGui = () => {
 
   modify.open();
 
-  //console.log("CHECKING ACTIVE", active)
   if (currentObject()) {
     modify
-      .add(currentObject().position, "y")
-      .min(-3)
-      .max(3)
-      .step(0.01)
-      .name("elevation")
-      .listen();
-    modify
       .add(currentObject().position, "x")
-      .min(-3)
-      .max(3)
-      .step(0.01)
-      .name("currentObject() side to side")
-      .listen();
-    modify
-      .add(currentObject().position, "x")
-      .min(-3)
-      .max(3)
-      .step(0.01)
+      .min(-500)
+      .max(500)
+      .step(1)
       .name("side to side")
       .listen();
     modify
       .add(currentObject().position, "z")
-      .min(-3)
-      .max(3)
-      .step(0.01)
+      .min(-500)
+      .max(500)
+      .step(1)
       .name("up and down")
       .listen();
+      modify
+          .add(axisCube.position, "z")
+          .min(-500)
+          .max(500)
+          .step(1)
+          .name("up and down screen")
+          .listen();
+      modify
+          .add(axisCube.position, "x")
+          .min(-500)
+          .max(500)
+          .step(1)
+          .name("side to side screen")
+          .listen();
+      modify
+          .add(localCube.position, "x")
+          .min(-500)
+          .max(500)
+          .step(1)
+          .name("side to side screen local")
+          .listen();
     modify
       .add(currentObject().rotation, "y")
       .min(-Math.PI * 2)
@@ -187,26 +288,33 @@ const createGui = () => {
       .step(Math.PI / 4)
       .name("rotate")
       .listen();
+      modify
+          .add(axisCube.rotation, "y")
+          .min(-Math.PI * 12)
+          .max(Math.PI * 12)
+          .step(Math.PI / 8)
+          .name("rotate Axis")
+          .listen();
     modify
       .addColor(parameters, "color")
       .onChange(() => {
-        initMaterial.color.set(parameters.color);
+          objects[objects.length - 1].material.color.set(parameters.color)
 
         console.log("CHECKING CURRENT OBJ", currentObject());
-        // CreateParameters.Color.set(parameters.color)
+
       })
       .listen();
   }
 
   let deleteObject = gui.addFolder("Delete");
   deleteObject.open();
-  deleteObject.add(parameters, "deleteLast");
+  deleteObject.add(parameters, "delete");
   deleteObject.add(parameters, "clear");
 
-  // gui.add(null,'delete current')
+
 };
 
-/* TRIGGER INITIALIZATION & FIRST RENDER */
+
 
 init();
 //render();
@@ -221,28 +329,12 @@ function init() {
     10000
   );
   camera.position.set(0, 1500, 0);
-  camera.lookAt(0, 0, 0);
+  //camera.lookAt(0, 0, 0);
+  //let unSpin = camera.rotation.y * -1
+  //camera.rotation._onChange(()=> axisCube.rotation.set(0,unSpin,0))
 
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x000000);
 
-  // roll-over helpers
-
-  const rollOverGeo = new THREE.BoxGeometry(50, 50, 50);
-  rollOverMaterial = new THREE.MeshBasicMaterial({
-    color: 0xff0000,
-    opacity: 0.5,
-    transparent: true,
-  });
-  rollOverMesh = new THREE.Mesh(rollOverGeo, rollOverMaterial);
-  scene.add(rollOverMesh);
-
-  // cubes
-
-  cubeGeo = new THREE.BoxGeometry(50, 50, 50);
-  cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xfeb74c }); //map: new THREE.TextureLoader().load( 'textures/square-outline-textured.png' ) } );
-
-  // grid
+    // grid
 
   const gridHelper = new THREE.GridHelper(1000, 20);
   scene.add(gridHelper);
@@ -255,22 +347,10 @@ function init() {
   const geometry = new THREE.PlaneGeometry(1000, 1000);
   geometry.rotateX(-Math.PI / 2);
 
-  plane = new THREE.Mesh(
-    geometry,
-    new THREE.MeshBasicMaterial({ visible: false })
+  plane = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ visible: false })
   );
   scene.add(plane);
-
   objects.push(plane);
-
-  // lights
-
-  const ambientLight = new THREE.AmbientLight(0x606060);
-  scene.add(ambientLight);
-
-  const directionalLight = new THREE.DirectionalLight(0xffffff);
-  directionalLight.position.set(1, 0.75, 0.5).normalize();
-  scene.add(directionalLight);
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
@@ -280,13 +360,7 @@ function init() {
   createGui();
   document.body.appendChild(renderer.domElement);
 
-  //document.addEventListener("pointermove", onPointerMove);
-  //document.addEventListener("pointerdown", onPointerDown);
-  //document.addEventListener("keydown", onDocumentKeyDown);
-  //document.addEventListener("keyup", onDocumentKeyUp);
 
-  //
-  //controls.addEventListener("change", render);
   window.addEventListener("resize", onWindowResize);
 }
 
@@ -296,37 +370,8 @@ function onWindowResize() {
 
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-  //render();
 }
 
-/* function onPointerMove(event) {
-        pointer.set(
-          (event.clientX / window.innerWidth) * 2 - 1,
-          -(event.clientY / window.innerHeight) * 2 + 1
-        );
-
-        raycaster.setFromCamera(pointer, camera);
-
-        const intersects = raycaster.intersectObjects(objects, false);
-
-        if (intersects.length > 0) {
-          const intersect = intersects[0];
-
-          rollOverMesh.position
-            .copy(intersect.point)
-            .add(intersect.face.normal);
-          rollOverMesh.position
-            .divideScalar(50)
-            .floor()
-            .multiplyScalar(50)
-            .addScalar(25);
-
-
-        }
-
-        render();
-      } */
 
 function onPointerDown(event) {
   pointer.set(
@@ -340,78 +385,183 @@ function onPointerDown(event) {
 
   if (intersects.length > 0) {
     const intersect = intersects[0];
-
-    // delete cube
-
-    if (isShiftDown) {
-      if (intersect.object !== plane) {
-        scene.remove(intersect.object);
-
-        objects.splice(objects.indexOf(intersect.object), 1);
-      }
-
-      // create cube
-    } else {
-      /* const voxel = new THREE.Mesh(cubeGeo, cubeMaterial);
-            voxel.position.copy(intersect.point).add(intersect.face.normal);
-            voxel.position
-              .divideScalar(50)
-              .floor()
-              .multiplyScalar(50)
-              .addScalar(25);
-            scene.add(voxel);
-
-            objects.push(voxel); */
-
       createObject(intersect);
-    }
 
-    isCreationMode = false;
     document.removeEventListener("pointerdown", onPointerDown);
 
     createGui();
 
     createControls();
-    //render();
   }
 }
 
-/* function onDocumentKeyDown(event) {
-        switch (event.keyCode) {
-          case 16:
-            isShiftDown = true;
-            break;
-        }
-      }
 
-      function onDocumentKeyUp(event) {
-        switch (event.keyCode) {
-          case 16:
-            isShiftDown = false;
-            break;
-        }
-      } */
+
+function toScreenSpin(direction) {
+
+    const obj = objects[objects.length-1]
+
+    //const obj2 = objects[objects.length-2]
+    // const vector = new THREE.Vector3();
+    // vector.setFromMatrixPosition( obj.matrixWorld );
+    // const widthHalf = (window.innerWidth/2);
+    // const heightHalf = (window.innerHeight/2);
+    // vector.project(camera);
+
+    if (direction === 'up'){
+
+
+        axisCube.add(obj)
+
+        axisCube.translateZ(10)
+        //localCube.translateZ(-10)
+        //objGroup.add(obj)
+        //localCube.add(obj)
+
+        //axisCube.remove(obj)
+        //localCube.add(obj)
+    }
+    else if (direction === 'down'){
+        axisCube.add(obj)
+
+        axisCube.geometry.translate( 0, 0, -5 );
+        //objects.add(obj)
+        //axisCube.geometry.translate( 0, 0, 50 );
+
+    }
+    else if (direction === 'left'){
+        axisCube.add(obj)
+
+        axisCube.geometry.translate( 50, 0, 0 );
+        //objects.add(obj)
+        axisCube.geometry.translate( -50, 0, 0 );
+
+
+    } else if (direction === 'right'){
+        axisCube.add(obj)
+
+        axisCube.geometry.translate( -50, 0, 0 );
+        //objects.add(obj)
+        axisCube.geometry.translate( 50, 0, 0 );
+
+    }
+}
+
 
 function render() {
-  console.log("WE ARE RENDENRING A NEW FRAME", scene);
+  console.log("RENDERING A NEW FRAME", scene);
 
   renderer.render(scene, camera);
 }
 
-const update = () => {
-  //const elapsedTime = clock.getElapsedTime();
 
-  // Update controls
+/////// UPDATE FUNCTION /////////
+/////// UPDATE FUNCTION /////////
+/////// UPDATE FUNCTION /////////
+
+
+
+
+
+const update = () => {
+
+
+
+  axisBack()
   if (controls) controls.update();
 
-  // Update Current Object
-  //active = objects.children[objects.children.length - 1]
 
-  // Render
+
   renderer.render(scene, camera);
 
-  // Call tick again on the next frame
   window.requestAnimationFrame(update);
 };
 
+
 update();
+
+
+
+
+
+
+
+
+
+/////// SCREEN SPACE TEST /////////
+/////// SCREEN SPACE TEST /////////
+/////// SCREEN SPACE TEST /////////
+
+
+// function screenSpace(obj){
+//
+//     const vector = obj.clone();
+//     let windowWidth = window.innerWidth;
+//     const minWidth = 1280;
+//
+//     if(windowWidth < minWidth) {
+//         windowWidth = minWidth;
+//     }
+//     console.log(obj)
+//     const widthHalf = (windowWidth/2);
+//     const heightHalf = (window.innerHeight/2);
+//
+//     vector.project(camera);
+//
+//     vector.x = ( vector.x * widthHalf ) + widthHalf;
+//     vector.y = - ( vector.y * heightHalf ) + heightHalf;
+//     vector.z = 0;
+//
+//     console.log(vector)
+//     console.log(objects[1].position)
+//
+//     return vector;
+//
+// };
+
+// function toScreenXYZ(direction)
+// {
+//     const obj = objects[objects.length-1]
+//     const vector = new THREE.Vector3();
+//     vector.setFromMatrixPosition( obj.matrixWorld );
+//     const widthHalf = (window.innerWidth/2);
+//     const heightHalf = (window.innerHeight/2);
+//     vector.project(camera);
+//     if (direction === 'up'){
+//         //vector.x = ( vector.x * widthHalf ) + widthHalf;
+//         obj.position.z =  (( vector.y * heightHalf ) + heightHalf) * -1
+//     }
+//     else if (direction === 'down'){
+//         vector.x = ( vector.x * widthHalf ) + widthHalf;
+//         vector.y = - ( vector.y * heightHalf ) + heightHalf;
+//     }
+//     else if (direction === 'left'){
+//         vector.x = ( vector.x * widthHalf ) + widthHalf;
+//         vector.y = - ( vector.y * heightHalf ) + heightHalf;
+//
+//     } else if (direction === 'right'){
+//         vector.x = ( vector.x * widthHalf ) + widthHalf;
+//         vector.y = - ( vector.y * heightHalf ) + heightHalf;
+//     }
+//     //vector.x = ( vector.x * widthHalf ) + widthHalf;
+//     //vector.y = - ( vector.y * heightHalf ) + heightHalf;
+//     console.log(vector)
+//     console.log(obj)
+//     return vector;
+// };
+
+
+
+
+
+
+
+
+
+
+/////// SCREEN SPACE TEST /////////
+
+
+
+
+
